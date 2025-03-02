8000646975150cdd322c4d8f105644961b51c41d
kate <c.k4terin4@mail.ru>
2025-03-02 22:05:50 +0300

    2

--- lab_1/task1.py
+++ lab_1/task1.py
@@ -10,116 +10,61 @@ x0 = 0, u(x0) = u0 = 1, u'(x0) = u'0 =  -0.5
 EPS = 1e-7
 accuracy = 5
 
-class Euler:
-    x_arr = [0]     # направо
-    y_arr = [1]     # направо
-    dy_arr = [-0.5] # направо
-
-    def __init__(self, h):
-        self.h = h
-
-    def x(self, ind):
-        while len(self.x_arr) <= ind:
-            self.x_arr.append(self.x(ind - 1) + self.h)
-        return self.x_arr[ind]
-
-    def y(self, ind):
-        while len(self.y_arr) <= ind:
-            self.y_arr.append(self.y(ind - 1) + self.h * self.dy(ind - 1))
-        return self.y_arr[ind]
-
-    def dy(self, ind):
-        while len(self.dy_arr) <= ind:
-            self.dy_arr.append(self.dy(ind - 1) + self.h * self.ddy(ind - 1))
-        return self.dy_arr[ind]
-
-    def ddy(self, ind):
-        if (abs(-2 * self.dy(ind) - self.y(ind)) < EPS and 
-            abs(4 * self.x(ind)) < EPS):
-            return 1 / 12   # правило Лапиталя, посчитано на листе
-        if self.x(ind) == 0:
-            print(ind, self._x_arr)
-        return (-2 * self.dy(ind) - self.y(ind)) / (4 * self.x(ind))
-
-    def get_x(self, left_cnt, right_cnt):
-        x = [self.x_arr[0] + self.h * i for i in range(-left_cnt, right_cnt + 1)]
-        return x
-
-    def get_y(self, left_cnt, right_cnt):
-        ry = [self.y(i) for i in range(right_cnt + 1)]
-        ly = [self.y(-i) for i in range(1, left_cnt + 1)]
-        ly.reverse()
-        return ly + ry
-    
-    def get_dy(self, left_cnt, right_cnt):
-        ry = [self.dy(i) for i in range(right_cnt + 1)]
-        ly = [self.dy(-i) for i in range(1, left_cnt + 1)]
-        ly.reverse()
-        return ly + ry
-    
-    def get_ddy(self, left_cnt, right_cnt):
-        _ddy_arr = []
-        ddy_arr = []
-        for i in range(right_cnt + 1):
-            ddy_arr.append(self.ddy(i))
-        
-        for i in range(1, left_cnt + 1):
-            _ddy_arr.append(self.ddy(-i))
-        
-        return list(reversed(_ddy_arr)) + ddy_arr
-
-
 
 def Tailor(x):
     return 1 - x / 2 + x ** 2 / 24 - x ** 3 / 720
 
-import sys
 
-if __name__ == '__main__':
-    print(1.7976931348623157e+308)
-    print(sys.float_info)
-    input()
-    print_tab = False
-    st = -5
-    end = 5
-    h = 1e-1
-    cnt = round((end - st) / h)
-    print(f'cnt = {cnt * 2 + 1}')
+def task1():
+    x0 = 0
+    y0 = 1
+    dy0 = -0.5
+
+    def ddyi(xi, yi, dyi):
+        if -2 * dyi - yi == 0 and xi == 0:
+            return 1 / 12
+        return (-2 * dyi - yi) / (4 * xi)
+
+    def dyip1(xi, yi, dyi, h):
+        return dyi + h * ddyi(xi, yi, dyi)
+
+    def yip1(yi, dyi, h):
+        return yi + h * dyi
     
-    euler = Euler(h)
-    xvals = euler.get_x(cnt, cnt)
-    tyvals = [Tailor(xi) for xi in xvals]
-    eyvals = euler.get_y(cnt, cnt)
-    edyvals = euler.get_dy(cnt, cnt)
-    eddyvals = euler.get_ddy(cnt, cnt)
-    if print_tab:
-        print('Euler')
-        tab = PrettyTable()
-        tab.field_names = ['x', 'y', 'dy', 'ddy']
-        for i in range(len(xvals)):
-            row = [f'{xvals[i]:.{accuracy}f}', f'{eyvals[i]:.{accuracy}f}', 
-                    f'{edyvals[i]:.{accuracy}f}', f'{eddyvals[i]:.{accuracy}f}']
-            tab.add_row(row)
-        print(tab)
+    xvals = [x0]
+    eyvals = [y0]
+    edyvals = [dy0]
+    tyvals = [Tailor(x0)]
+    funcvals = [y0]
+    ravals = [0]
 
-        print(Tailor)
-        tab = PrettyTable()
-        tab.field_names = ['x', 'y']
-        for i in range(len(xvals)):
-            row = [f'{xvals[i]:.{accuracy}f}', f'{tyvals[i]:.{accuracy}f}']
-            tab.add_row(row)
-        print(tab)
+    hcur = 1e-5
+    xmax = 15
+    while xvals[-1] < xmax:
+    # while ravals[-1] < 1e-4:
+        xi = xvals[-1] + hcur
+        yi = yip1(eyvals[-1], edyvals[-1], hcur)
+        dyi = dyip1(xvals[-1], eyvals[-1], edyvals[-1], hcur)
 
-    tab = PrettyTable()
-    tab.field_names = ['x', 'y Euler', 'y Tailor', 'diff']
-    for i in range(len(xvals)):
-        row = [f'{xvals[i]:.{accuracy}f}', f'{eyvals[i]:.{accuracy}f}', 
-                f'{tyvals[i]:.{accuracy}f}', f'{eyvals[i] - tyvals[i]:.{accuracy}f}']
-        tab.add_row(row)
-    print(tab)
+        xvals.append(xi)
+        eyvals.append(yi)
+        edyvals.append(dyi)
+        tyvals.append(Tailor(xvals[-1]))
+        ravals.append(abs(eyvals[-1] - tyvals[-1]) / tyvals[-1])
+        funcvals.append(m.cos(m.sqrt(xvals[-1])))
+    print(f'cnt = {len(xvals)}')
+
+    # tab = PrettyTable()
+    # tab.field_names = ['x', 'y Euler', 'y Tailor', 'relative accuracy']
+    # for i in range(len(xvals)):
+    #     row = [f'{xvals[i]:.{accuracy}f}', f'{eyvals[i]:.{accuracy}f}', 
+    #             f'{tyvals[i]:.{accuracy}f}', f'{ravals[i]:.{accuracy}f}']
+    #     tab.add_row(row)
+    # print(tab)
 
     plt.plot(xvals, tyvals, color='r', label='Tailor y(x)')
     plt.plot(xvals, eyvals, color='b', label='Euler y(x)')
+    plt.plot(xvals, funcvals, color='g', label='cos(sqrt(x))')
     # plt.plot(xvals, edyvals, color='g', label='Euler dy')
     # plt.plot(xvals, eddyvals, color='y', label='Euler ddy')
     plt.xlabel('Ось х')  # Подпись для оси х
@@ -127,7 +72,12 @@ if __name__ == '__main__':
     plt.title('График')  # Название
     plt.legend()
     plt.show()
-    
+
+
+import sys
+
+if __name__ == '__main__':
+    task1()
 
 
     
--- lab_1/task3.py
+++ lab_1/task3.py
@@ -2,7 +2,7 @@ from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 import numpy as np
 
-EPS = 1e-7
+EPS = 1e-20
 accuracy = 5
 
 class Euler:
@@ -61,8 +61,6 @@ def Picar1(x):
 def Picar2(x):
     return (x ** 2) / 2 + (x ** 7) / 56
 
-
-
 def Picar3(x):
     return ((x**(22))/(3863552) + 
             (3*x**(17))/(106624) + 
@@ -86,18 +84,61 @@ def Picar4(x):
             (x**7)/(56)+
             (x**2)/2)
     
+def task3():
+    x0, y0 = 0, 0
 
-if __name__ == '__main__':
-    st = 0
-    end = 2
-    h = 0.05
-    lcnt = round((0 - st) / h)
-    rcnt = round((end - 0) / h)
-    print(f'cnt = {lcnt + rcnt}')
+    def dyi(xi, yi):
+        return xi + yi ** 3
 
-    euler = Euler(h)
-    xvals = euler.get_x(lcnt, rcnt)
-    eyvals = euler.get_y(lcnt, rcnt)
+    def yip1(xi, yi, h):
+        return yi + h * dyi(xi, yi)
+
+    def yip1_2h(xi, yi, h):
+        yih = yip1(xi, yi, h)
+        xih = xi + h
+        return yip1(xih, yih, h)
+
+    # find xmax for hcur
+    hcur = 1e-100
+    rel_accuracy = 0.0001
+    xvals = [x0]
+    eyvals = [y0]
+    eyh2vals = [y0]
+
+    for i in range(3):
+        xi = xvals[-1] + hcur
+        yi = yip1(xvals[-1], eyvals[-1], hcur)
+        # yi2 = yip1_2h(xvals[-1], eyvals[-1], hcur / 2)
+        yi2 = Picar4(xvals[-1] + hcur)
+        xvals.append(xi)
+        eyvals.append(yi)
+        eyh2vals.append(yi2)
+    
+    yh = yip1(xvals[-1], eyvals[-1], hcur)
+    # yh2 = yip1_2h(xvals[-1], eyvals[-1], hcur / 2)
+    yh2 = Picar4(xvals[-1] + hcur)
+    print(eyvals)
+    print(eyh2vals)
+
+    print(abs(yh - yh2) / yh2)
+    while abs(yh - yh2) / yh2 <= rel_accuracy:
+        try:
+            yh = yip1(xvals[-1], eyvals[-1], hcur)
+            # yh2 = yip1_2h(xvals[-1], eyvals[-1], hcur / 2)
+            yh2 = Picar4(xvals[-1] + hcur)
+
+            eyh2vals.append(yh2)
+            eyvals.append(yh)
+            xvals.append(xvals[-1] + hcur)
+            # print(eyvals)
+            # print(eyh2vals)
+            
+            if yh2 != 0:
+                print(abs((yh - yh2) / yh2))
+        except OverflowError:
+            break
+
+    # end find xmax for hcur
 
     p1yvals = [Picar1(xi) for xi in xvals]
     p2yvals = [Picar2(xi) for xi in xvals]
@@ -106,15 +147,15 @@ if __name__ == '__main__':
 
 
     tab = PrettyTable()
-    tab.field_names = ['x', 'y Euler', 'y Picar1', 'y Picar2', 'y Picar3', 'y Picar4']
+    tab.field_names = ['yh2', 'x', 'y Euler', 'y Picar1', 'y Picar2', 'y Picar3', 'y Picar4']
     for i in range(len(xvals)):
-        row = [f'{xvals[i]:.{accuracy}f}', f'{eyvals[i]:.{accuracy}f}',
+        row = [f'{eyh2vals[i]:.{accuracy}f}', f'{xvals[i]:.{accuracy}f}', f'{eyvals[i]:.{accuracy}f}',
                f'{p1yvals[i]:.{accuracy}f}', f'{p2yvals[i]:.{accuracy}f}',
                f'{p3yvals[i]:.{accuracy}f}', f'{p4yvals[i]:.{accuracy}f}']
         tab.add_row(row)
     print(tab)
 
-
+    print(f'xmax = {xvals[-1]}')
     plt.plot(xvals, eyvals, color='r', marker='.', label='Euler y(x)')
     plt.plot(xvals, p1yvals, color='g', marker=',', label='Picar1 y(x)')
     plt.plot(xvals, p2yvals, color='b', marker='o', label='Picar2 y(x)')
@@ -125,5 +166,75 @@ if __name__ == '__main__':
     plt.title('График')  # Название
     plt.legend()
     plt.show()
+
+
+def break_point():
+    x0, y0 = 0, 0
+
+    def dyi(xi, yi):
+        return xi + yi ** 3
+
+    def yip1(xi, yi, h):
+        return yi + h * dyi(xi, yi)
+
+    def yip1_2h(xi, yi, h):
+        yih = yip1(xi, yi, h)
+        xih = xi + h
+        return yip1(xih, yih, h)
+
+    hcur = 1e-2
+    rel_accuracy = 1e-4
+    xvals = [x0, x0 + hcur]
+    eyvals = [y0, yip1(x0, y0, hcur)]
+    # eyh2vals = [y0, yip1(x0, y0, hcur)]
+
+    while eyvals[-1] != float('inf'): 
+        try:
+            yh = yip1(xvals[-1], eyvals[-1], hcur)
+            yh2 = yip1_2h(xvals[-1], eyvals[-1], hcur / 2)
+            while abs(yh - yh2) / yh2 >= rel_accuracy:
+                hcur /= 2
+                yh = yip1(xvals[-1], eyvals[-1], hcur)
+                yh2 = yip1_2h(xvals[-1], eyvals[-1], hcur / 2)
+                # print(f'h = {hcur}, {abs(yh - yh2) / yh2}, {yh}, {yh2}')
+            eyvals.append(yh)
+            xvals.append(xvals[-1] + hcur)
+            # print(xvals[-1], eyvals[-1], sep='\t')
+        except OverflowError:
+            break
+
+    print(f'xmax = {xvals[-1]}')
+    print(f'hcur = {hcur}')
+    print(f'n = {len(xvals)}')
+
+    # accuracy = 10
+    # tab = PrettyTable()
+    # tab.field_names = ['x', 'y Euler']
+    # for i in range(len(xvals)):
+    #     row = [f'{xvals[i]:.{accuracy}f}', f'{eyvals[i]:.{accuracy}f}']
+    #     tab.add_row(row)
+    # print(tab)
+
+    
+    # plt.plot(xvals, eyvals, color='r', marker='.', label='Euler y(x)')
+    # plt.xlabel('Ось х')  # Подпись для оси х
+    # plt.ylabel('Ось y')  # Подпись для оси y
+    # plt.title('График')  # Название
+    # plt.legend()
+    # plt.show()
+
+
+def info():
+    import sys
+    print(sys.float_info)
+    '''sys.float_info(max=1.7976931348623157e+308, max_exp=1024, 
+    max_10_exp=308, min=2.2250738585072014e-308, min_exp=-1021, 
+    min_10_exp=-307, dig=15, mant_dig=53, epsilon=2.220446049250313e-16, 
+    radix=2, rounds=1)'''
+
+if __name__ == '__main__':
+    # info()
+    task3()
+    # break_point()
 
 
